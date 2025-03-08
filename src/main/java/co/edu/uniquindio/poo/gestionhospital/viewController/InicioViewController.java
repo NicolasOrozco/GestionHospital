3da33eb4dca81ecb9efadeab32b00d24f9489d58
lythraa <lythraa@gmail.com>
2025-03-07 22:44:05 -0500

    update

---
+++ src/main/java/co/edu/uniquindio/poo/gestionhospital/viewController/InicioViewController.java
@@ -0,0 +1,149 @@
+package co.edu.uniquindio.poo.gestionhospital.viewController;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class InicioViewController {
+
+    @FXML
+    private Button btnCancelarCita;
+
+    @FXML
+    private Button btnEditarPaciente;
+
+    @FXML
+    private TableColumn<?, ?> colDoctorCita;
+
+    @FXML
+    private TextArea txtFacturacion;
+
+    @FXML
+    private TableColumn<?, ?> colEspecialidad;
+
+    @FXML
+    private Button btnAgregarPaciente;
+
+    @FXML
+    private Button btnEditarDoctor;
+
+    @FXML
+    private TableColumn<?, ?> colEdad;
+
+    @FXML
+    private TableView<?> tablaCitas;
+
+    @FXML
+    private Button BtnReservarCita;
+
+    @FXML
+    private TableColumn<?, ?> colNombre;
+
+    @FXML
+    private TableColumn<?, ?> colHistorial;
+
+    @FXML
+    private TableColumn<?, ?> colFechaCita;
+
+    @FXML
+    private Button bntEliminarPaciente;
+
+    @FXML
+    private TableColumn<?, ?> colPacienteCita;
+
+    @FXML
+    private TextField txtHorario;
+
+    @FXML
+    private TableColumn<?, ?> colNombreDoctor;
+
+    @FXML
+    private Button btnAgregarDoctor;
+
+    @FXML
+    private TableView<?> tablaDoctores;
+
+    @FXML
+    private TableView<?> tablaPacientes;
+
+    @FXML
+    private Button btnEliminarDoctor;
+
+    @FXML
+    private TextField txtMaxPacientes;
+
+    @FXML
+    void agregarPaciente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editarPaciente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eliminarPaciente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void agregarDoctor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editarDoctor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eliminarDoctor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reservarCita(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cancelarCita(ActionEvent event) {
+
+    }
+
+    @FXML
+    void guardarConfiguracion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void salir(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mostrarPacientes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mostrarDoctores(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mostrarCitas(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mostrarConfiguracion(ActionEvent event) {
+
+    }
+
+}
